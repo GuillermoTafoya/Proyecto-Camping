91b5520eee1b9ba1c28c4df9744d5665a0028aaf
Guillermo Tafoya <A01633790@itesm.mx>
2020-12-10 10:41:54 -0600

    Final

--- Proyecto Camping.cpp
+++ Proyecto Camping.cpp
@@ -4,18 +4,17 @@
 #include <iomanip>
 #include <assert.h>
 #include <string>
+#include <algorithm>
+
+bool is_empty(std::ifstream& pFile);
+void flush();
 
 //Borra la terminal
 void flush() {
     system("CLS");
 }
 
-//Guarda el vector de productos en el archivo
-void save() {
-    std::cout << "Guardando....\n";
-    //Falta trabajo jeje
-}
-
+//Clase que modela un producto
 class producto {
     public:
         int stock, id;
@@ -49,9 +48,33 @@ class producto {
         }
 };
 
+std::vector<producto> load();
+
+void describe(std::vector<producto> inventario);
+
+//Guarda el vector de productos en el archivo
+void save(std::vector<producto> &inventario) {
+    std::ofstream file_obj;
+    file_obj.open("inventario.txt", std::ios::trunc);
+    std::cout << "Guardando....\n";
+
+
+    file_obj << std::setw(30) << std::left << "ID" << std::setw(30) << "Producto" << std::setw(30) << "Stock" << "Caducidad";
+
+    for (int i = 0; i < inventario.size(); i++) {
+        inventario[i].id = i;
+        file_obj << inventario[i];
+    }
+
+    
+
+    std::cout << "Proceso terminado.\n";
+    std::cout << std::endl << "Presione ENTER para continuar...";
+    std::cin.get();
+}
 
 //Añade productos al archivo.
-void add() {
+void add(std::vector<producto> &inventario) {
     flush();
     std::string name, caducidad, centinela;
     int stock;
@@ -65,13 +88,10 @@ void add() {
         id++;
     file.close();
 
-    std::ofstream file_obj;
-    file_obj.open("inventario.txt", std::ios::app);
-
     while (true) {
 
         std::cout << "Para dejar de ingresar productos, escriba '-1' en cualquier momento.\n";
-        std::cout << "Ingrese el nombre del producto (no usar espacios): ";
+        std::cout << "Ingrese el nombre del producto (no use espacios): ";
 
         std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -80,7 +100,7 @@ void add() {
         std::getline(std::cin, name);
         while (name.find(' ') != std::string::npos){
         
-            std::cout << "Ingrese el nombre del producto (no usar espacios): ";
+            std::cout << "Ingrese el nombre del producto (no use espacios): ";
             std::getline(std::cin, name);
         }
         if (name == "-1") {
@@ -91,7 +111,7 @@ void add() {
         if (stock == -1){
             break;
         }
-        std::cout << "Ingrese la fecha de caducidad del producto: ";
+        std::cout << "Ingrese la fecha de caducidad del producto (no use espacios): ";
         std::cin >> caducidad;
         if (caducidad == "-1") {
             break;
@@ -101,24 +121,326 @@ void add() {
         producto nuevo(name, caducidad, stock, id);
 
         id++;
-        file_obj << nuevo;
+        inventario.push_back(nuevo);
     }
 
-    file_obj.close();
+}
+
+//Regresa el indice donde se encuentra un producto
+auto search_by_name(std::vector<producto> &inventario, std::string name) {
+
+    auto it = find_if(inventario.begin(), inventario.end(), [&name](const producto& obj) {return obj.name == name; });
+    
+
+        if (it != inventario.end())
+        {
+            auto index = std::distance(inventario.begin(), it);
+            return index;
+        }
+        return INT_MAX;
 }
 
 //Elimina elementos del vector que contiene los productos y posteriormente guarda la versión modificada al archivo.
-void obliterate() {
-    std::cout << "Baja.\n";
-    save();
+void obliterate(std::vector<producto> &inventario) {
+
+    if (inventario.empty()) {
+        flush();
+        std::cout << "El inventario está vacio.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << std::endl << "Presione ENTER para continuar...";
+        std::cin.get();
+        return;
+    }
+
+    int action;
+    std::string name;
+    int i;
+    do {
+        flush();
+        
+        std::cout << "Elija una de las siguientes opciones:\n";
+        std::cout << "1: Borrar buscando por nombre\n";
+        std::cout << "2: Borrar buscando por id\n";
+        std::cout << "3: Salir\n\n";
+        std::cin >> action;
+        std::cin.clear();
+
+        
+
+        switch (action) {
+        default:
+            std::cout << "Comando no reconocido. Intentelo de nuevo.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << std::endl << "Presione ENTER para continuar...";
+            std::cin.get();
+            break;
+        case 1:
+            
+            describe(inventario);
+
+            std::cout << "\nIngresa el producto que desea eliminar: ";
+            std::cin >> name;
+            i = search_by_name(inventario, name);
+            if (i != INT_MAX) {
+                
+                inventario.erase(inventario.begin()+i);
+                std::cout << "El producto <" << name << " > ha sido eliminado.\n";
+                save(inventario);
+                std::cin.get();
+            }
+            else {
+                std::cout << "No existe un producto con el nombre <" << name << "> por el momento.";
+                std::cout << std::endl << "Presione ENTER para continuar...";
+                std::cin.get();
+                std::cin.get();
+            }
+            
+            break;
+        case 2:
+            describe(inventario);
+            std::cout << "\nIngresa el id del producto que desea eliminar: ";
+            
+            std::cin >> i;
+
+            if (i<0 || i>inventario.size() - 1) {
+                std::cout << "ID invalido.";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << std::endl << "Presione ENTER para continuar...";
+                std::cin.get();
+                break;
+            }
+
+            name = inventario[i].name;
+            inventario.erase(inventario.begin() + i);
+            std::cout << "El producto <" << name << "> ha sido eliminado.\n";
+            save(inventario);
+            std::cin.get();
+            break;
+        case 3:
+            flush();
+            break;
+        }
+
+
+
+    } while (action != 3);
+    save(inventario);
+    std::cin.get();
 }
 
 //Modifica elementos del vector que contiene los productos y posteriormente guarda la versión modificada al archivo.
-void change() {
-    std::cout << "Cambio.\n";
-    save();
-}
+void change(std::vector<producto> &inventario) {
 
+
+    if (inventario.empty()) {
+        flush();
+        std::cout << "El inventario está vacio.\n";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << std::endl << "Presione ENTER para continuar...";
+        std::cin.get();
+
+        return;
+    }
+
+
+
+    int action, action2, newID;
+    std::string name, newAttribute;
+    int i;
+    do {
+        flush();
+        std::cout << "Elija una de las siguientes opciones:\n";
+        std::cout << "1: Editar buscando por nombre\n";
+        std::cout << "2: Editar buscando por id\n";
+        std::cout << "3: Salir\n\n";
+        std::cin >> action;
+        action2 = 0;
+        std::cin.clear();
+
+        
+
+        switch (action) {
+        default:
+            std::cout << "Comando no reconocido. Intentelo de nuevo.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << std::endl << "Presione ENTER para continuar...";
+            std::cin.get();
+            break;
+        case 1:
+
+            describe(inventario);
+
+            std::cout << "\nIngresa el producto que desea editar: ";
+            std::cin >> name;
+            i = search_by_name(inventario, name);
+            if (i != INT_MAX) {
+
+
+                while (action2 != 4) {
+
+                    flush();
+                    std::cout << "Que busca editar del producto <" << name << "> ?\n";
+                    std::cout << "1: Editar nombre\n";
+                    std::cout << "2: Editar stock\n";
+                    std::cout << "3: Editar caducidad\n";
+                    std::cout << "4: Regresar\n";
+
+                    std::cin >> action2;
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                    switch (action2) {
+                    default:
+                        std::cout << "Comando no reconocido. Intentelo de nuevo.\n";
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        std::cout << std::endl << "Presione ENTER para continuar...";
+                        std::cin.get();
+                        break;
+                    case 1:
+
+                        std::cout << "Ingrese el nuevo nombre del producto (no use espacios): ";
+                        std::getline(std::cin, newAttribute);
+                        while (newAttribute.find(' ') != std::string::npos) {
+
+                            std::cout << "Ingrese el nuevo nombre del producto (no use espacios): ";
+                            std::getline(std::cin, newAttribute);
+                        }
+
+                        inventario[i].name = newAttribute;
+                        save(inventario);
+                        std::cin.get();
+
+                        break;
+                    case 2:
+
+
+                        std::cout << "Ingrese el nuevo stock: ";
+                        std::cin >> newID;
+
+                        inventario[i].stock = newID;
+                        save(inventario);
+                        std::cin.get();
+
+                        break;
+                    case 3:
+                        std::cout << "Ingrese la fecha de caducidad del producto (no use espacios): ";
+                        std::cin >> newAttribute;
+
+                        inventario[i].caducidad = newAttribute;
+                        save(inventario);
+                        std::cin.get();
+                        break;
+                    case 4:
+                        break;
+                    }
+                }
+
+
+            }
+            else {
+                std::cout << "No existe un producto con el nombre <" << name << "> por el momento.";
+                std::cout << std::endl << "Presione ENTER para continuar...";
+                std::cin.get();
+                std::cin.get();
+                break;
+            }
+
+            break;
+        case 2:
+
+            describe(inventario);
+
+            std::cout << "\nIngresa el id del producto que desea modificar: ";
+            std::cin >> i;
+
+            if (i<0 || i>inventario.size() - 1) {
+                std::cout << "ID invalido.";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << std::endl << "Presione ENTER para continuar...";
+                std::cin.get();
+                break;
+            }
+
+            while (action2 != 4) {
+
+                flush();
+                std::cout << "Que busca editar del producto <" << inventario[i].name << "> ?\n";
+                std::cout << "1: Editar nombre\n";
+                std::cout << "2: Editar stock\n";
+                std::cout << "3: Editar caducidad\n";
+                std::cout << "4: Regresar\n";
+
+                std::cin >> action2;
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+                switch (action2) {
+                default:
+                    std::cout << "Comando no reconocido. Intentelo de nuevo.\n";
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    std::cout << std::endl << "Presione ENTER para continuar...";
+                    std::cin.get();
+                    break;
+                case 1:
+
+                    std::cout << "Ingrese el nuevo nombre del producto (no use espacios): ";
+                    std::getline(std::cin, newAttribute);
+                    while (newAttribute.find(' ') != std::string::npos) {
+
+                        std::cout << "Ingrese el nuevo nombre del producto (no use espacios): ";
+                        std::getline(std::cin, newAttribute);
+                    }
+
+                    inventario[i].name = newAttribute;
+                    save(inventario);
+
+                    break;
+                case 2:
+
+                    std::cout << "Ingrese el nuevo stock: ";
+                    std::cin >> newID;
+
+                    inventario[i].stock = newID;
+                    save(inventario);
+                    std::cin.get();
+
+                    break;
+                case 3:
+                    std::cout << "Ingrese la fecha de caducidad del producto (no use espacios): ";
+                    std::cin >> newAttribute;
+
+                    inventario[i].caducidad = newAttribute;
+                    save(inventario);
+                    std::cin.get();
+                    break;
+                case 4:
+                    break;
+                }
+            }
+
+        break;
+
+
+
+        case 3: {
+            flush();
+            break;
+            }
+        }
+           
+    } while (action != 3);
+
+    save(inventario);
+    std::cin.get();
+}
 
 //Crea un vector de los productos para guardar temporalmente la información en la RAM y facilitar su manipulación.
 std::vector<producto> load() {
@@ -128,8 +450,6 @@ std::vector<producto> load() {
     producto obj;
 
     std::getline(file_obj, helper);
-
-    int i = 0;
 
     while (!file_obj.eof()) {
         assert(file_obj >> obj);
@@ -156,9 +476,7 @@ void describe(std::vector<producto> inventario) {
     for (int i = 0; i < inventario.size(); i++) {
         inventario[i].describe();
     }
-    std::cout << std::endl << "Presione ENTER para continuar...";
-    std::cin.get();
-    getchar();
+    
 }
 
 //Comprueba si el archivo está vacio
@@ -198,31 +516,32 @@ int main()
             std::cin.get();
             break;
         case -1:
-            
             inventario = load();
-            std::cout << "Base de datos actualizada.\n";
             break;
         case 1:
-            add();
-            inventario = load();
+            add(inventario);
+            save(inventario);
             break;
         case 2:
-            obliterate();
+            obliterate(inventario);
+            
             break;
         case 3:
-            change();
+            change(inventario);
+            
             break;
         case 4:
             describe(inventario);
+            std::cout << std::endl << "Presione ENTER para continuar...";
+            std::cin.get();
+            std::cin.get();
             break;
         case 5:
-            save();
+            save(inventario);
             break;
         }
 
     } while (action != 5);
-
-    std::cout << "Proceso terminado.\n";
 
     return 0;
 }
